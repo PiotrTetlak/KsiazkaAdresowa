1825f535dbd901fc2247f767c3ebf111e607a2f2
Piotr Tetlak <piotrtetlak007@gmail.com--list>
2021-01-24 02:58:31 +0100

    Logowanie uzykownikow, dostep tylko do swoich danych

--- main.cpp
+++ main.cpp
@@ -14,6 +14,127 @@ struct Adresat {
     string imie, nazwisko, adres, telefon, mail;
 };
 
+struct Uzytkownik {
+    int id;
+    string nazwa, haslo;
+};
+
+void wyswietlMenuLogowania() {
+
+    system("cls");
+    cout<<"1. Rejestracja"<<endl;
+    cout<<"2. Logowanie"<<endl;
+    cout<<"9. Wyjdz z programu"<<endl;
+}
+
+void zarejestruj(vector<Uzytkownik> &uzytkownicy) {
+
+    Uzytkownik nowyUzytkownik;
+    string nazwa,haslo;
+    int ostatnieId=0;
+    int i=0;
+    cout<<"Podaj nazwe uzytkownika:"<<endl;
+    cin>>nazwa;
+
+    while(i<uzytkownicy.size()) {
+        if(uzytkownicy[i].nazwa==nazwa) {
+            cout<<"Taki uzytkownik istnieje, podaj inna nazwa"<<endl;
+            cin>>nazwa;
+            i=0;
+        } else {
+            i++;
+        }
+    }
+
+    if(uzytkownicy.size()>0) {
+        ostatnieId=uzytkownicy[uzytkownicy.size()-1].id;
+    } else {
+        ostatnieId=0;
+    }
+    cout<<"Podaj haslo:"<<endl;
+    cin>>haslo;
+
+    nowyUzytkownik.nazwa=nazwa;
+    nowyUzytkownik.haslo=haslo;
+    nowyUzytkownik.id=ostatnieId+1;
+
+    uzytkownicy.push_back(nowyUzytkownik);
+
+    cout<<"Uzytkownik zapisany"<<endl;
+    Sleep(1000);
+}
+
+int odczytajZPlikuUzytkownicy(vector<Uzytkownik> &uzytkownicy) {
+
+    fstream uzytkownicyPlik;
+    uzytkownicyPlik.open("uzytkownicy.txt", ios::in);
+
+    string linia;
+    string daneUzytkownika[3];
+    int nrlinii=1;
+    int i=0;
+    int j=0;
+
+
+    Uzytkownik uzytkownikZPliku;
+
+    while(getline(uzytkownicyPlik,linia)) {
+        daneUzytkownika[0]="";
+        daneUzytkownika[1]="";
+        daneUzytkownika[2]="";
+        j=0;
+        for(int i=0; i<linia.length(); i++) {
+            if(linia[i]=='|') {
+                daneUzytkownika[j].insert(0,linia,0,i);
+                linia.erase(0,i+1);
+                i=0;
+                j++;
+            }
+        }
+        uzytkownikZPliku.id=atoi(daneUzytkownika[0].c_str());
+        uzytkownikZPliku.nazwa=daneUzytkownika[1];
+        uzytkownikZPliku.haslo=daneUzytkownika[2];
+
+        uzytkownicy.push_back(uzytkownikZPliku);
+    }
+    uzytkownicyPlik.close();
+}
+
+void dopisanieDoPlikuUzytkownicy(vector<Uzytkownik> &uzytkownicy) {
+
+    int iloscUzytkownikow=uzytkownicy.size();
+
+    fstream uzytkownicyPlik;
+    uzytkownicyPlik.open("uzytkownicy.txt", ios::out | ios::app);
+    stringstream ss;
+    ss << uzytkownicy[iloscUzytkownikow-1].id;
+    string id = ss.str();
+
+    uzytkownicyPlik<<id+'|'<<uzytkownicy[iloscUzytkownikow-1].nazwa+'|'<<uzytkownicy[iloscUzytkownikow-1].haslo+'|'<<endl;
+    uzytkownicyPlik.close();
+}
+
+int logowanie(vector<Uzytkownik> &uzytkownicy) {
+
+    string nazwa,haslo;
+    cout<<"Podaj nazwe uzytkownika:"<<endl;
+    cin>>nazwa;
+    cout<<"Podaj haslo:"<<endl;
+    cin>>haslo;
+
+    for(int i=0; i<uzytkownicy.size(); i++) {
+        if(uzytkownicy[i].nazwa==nazwa && uzytkownicy[i].haslo==haslo) {
+            cout<<"Zalogowano pomyslnie"<<endl;
+            Sleep(1000);
+            return uzytkownicy[i].id;
+        }
+    }
+    cout<<"Nieprawidlowe dane, nastapi przejscie do menu glownego"<<endl;
+    Sleep(1000);
+    return 0;
+}
+//-------------------------------------------------------------------------
+
 void zapiszKontakt(vector<Adresat> &adresaci, int id) {
 
     Adresat nowyAdresat;
@@ -185,29 +306,42 @@ void edytujKontakt(vector<Adresat> &adresaci) {
     }
 }
 
-void usuniecieIEskportDoNowegoPliku(vector<Adresat> &adresaci) {
+string zmianaIntnaString(int liczba) {
 
+    stringstream ss;
+    ss << liczba;
+    string wyraz = ss.str();
+
+    return wyraz;
+}
+
+void usuniecieIEskportDoNowegoPliku(vector<Adresat> &adresaci, string numerUzytkownika) {
+
+    string id;
+    string nazwaPliku=numerUzytkownika+".txt";
     int iloscAdresow=adresaci.size();
-    remove("adresy.txt");
+    remove(nazwaPliku.c_str());
     fstream adresy;
-    adresy.open("adresy.txt", ios::out | ios::app);
+    adresy.open(nazwaPliku.c_str(), ios::out | ios::app);
     for(int i=0; i<iloscAdresow; i++) {
-        stringstream ss;
-        ss << adresaci[i].id;
-        string id = ss.str();
+
+        id=zmianaIntnaString(adresaci[i].id);
+
         adresy<<id+'|'<<adresaci[i].imie+'|'<<adresaci[i].nazwisko+'|'<<adresaci[i].adres+'|'<<adresaci[i].telefon+'|'<<adresaci[i].mail+'|'<<endl;
     }
     adresy.close();
 }
 
-void dopisanieDoPliku(vector<Adresat> &adresaci) {
+void dopisanieDoPliku(vector<Adresat> &adresaci, string numerUzytkownika) {
 
+    string id;
+    string nazwaPliku=numerUzytkownika+".txt";
     int iloscAdresow=adresaci.size();
     fstream adresy;
-    adresy.open("adresy.txt", ios::out | ios::app);
-    stringstream ss;
-    ss << adresaci[iloscAdresow-1].id;
-    string id = ss.str();
+    adresy.open(nazwaPliku.c_str(), ios::out | ios::app);
+
+    id=zmianaIntnaString(adresaci[iloscAdresow-1].id);
+
     adresy<<id+'|'<<adresaci[iloscAdresow-1].imie+'|'<<adresaci[iloscAdresow-1].nazwisko+'|'<<adresaci[iloscAdresow-1].adres+'|'<<adresaci[iloscAdresow-1].telefon+'|'<<adresaci[iloscAdresow-1].mail+'|'<<endl;
     adresy.close();
 }
@@ -221,10 +355,11 @@ bool sprawdzCzyPlikIstnieje( const char * plik ) {
     return true;
 }
 
-int odczytajZPliku(vector<Adresat> &adresaci) {
+int odczytajZPliku(vector<Adresat> &adresaci, string numerUzytkownika) {
 
+    string nazwaPliku=numerUzytkownika+".txt";
     fstream adresy;
-    adresy.open("adresy.txt", ios::in);
+    adresy.open(nazwaPliku.c_str(), ios::in);
     int ostatnieId;
 
     string linia;
@@ -280,18 +415,18 @@ void wyswietlMenuGlowne() {
     cout<<"4. Pokaz wszystkie kontakty"<<endl;
     cout<<"5. Usun kontakt"<<endl;
     cout<<"6. Edytuj kontakt"<<endl;
-    cout<<"9. Wyjdz z programu"<<endl;
+    cout<<"9. Wyjdz do logowania"<<endl;
 }
-
-int main() {
-
-    vector<Adresat> adresaci;
-
+void wejdzDoMenuGlownego(vector<Adresat> &adresaci, int idUzytkownika) {
     char wybor;
     int ostatnieId=0;
+    string numerUzytkownika;
 
-    if(sprawdzCzyPlikIstnieje("adresy.txt")==true) {
-        ostatnieId=odczytajZPliku(adresaci);
+    numerUzytkownika=zmianaIntnaString(idUzytkownika);
+    string nazwaPliku=numerUzytkownika+".txt";
+
+    if(sprawdzCzyPlikIstnieje(nazwaPliku.c_str())==true) {
+        ostatnieId=odczytajZPliku(adresaci, numerUzytkownika);
     }
 
     while(1) {
@@ -302,7 +437,7 @@ int main() {
         if(wybor=='1') {
             zapiszKontakt(adresaci, ostatnieId);
             ostatnieId=adresaci[adresaci.size()-1].id;
-            dopisanieDoPliku(adresaci);
+            dopisanieDoPliku(adresaci, numerUzytkownika);
         } else if(wybor=='2') {
             znajdzImie(adresaci);
         } else if(wybor=='3') {
@@ -310,25 +445,54 @@ int main() {
         } else if(wybor=='4') {
             wyswietlWszystkie(adresaci);
         } else if(wybor=='5') {
-
             if(adresaci.size()>0) {
                 usunKontakt(adresaci);
                 ostatnieId=adresaci[adresaci.size()-1].id;
-                usuniecieIEskportDoNowegoPliku(adresaci);
-            }
-
-            else {
+                usuniecieIEskportDoNowegoPliku(adresaci, numerUzytkownika);
+            } else {
                 cout<<"Brak adresatow do usuniecia"<<endl;
                 Sleep(1000);
             }
         } else if(wybor=='6') {
             edytujKontakt(adresaci);
-            usuniecieIEskportDoNowegoPliku(adresaci);
+            usuniecieIEskportDoNowegoPliku(adresaci, numerUzytkownika);
         } else if(wybor=='9') {
-            exit(0);
+            break;
         } else {
             cout<<"Wybierz poprawny przycisk (od 1 do 5)"<< endl;
             Sleep(2000);
+        }
+    }
+}
+
+int main() {
+
+    vector<Adresat> adresaci;
+    vector <Uzytkownik> uzytkownicy;
+
+    int idZalogowanegoUzytkownika;
+    char wyborLogowanie;
+
+    if(sprawdzCzyPlikIstnieje("uzytkownicy.txt")==true) {
+        odczytajZPlikuUzytkownicy(uzytkownicy);
+    }
+
+    while(1) {
+        adresaci.clear();
+        wyswietlMenuLogowania();
+
+        cin>>wyborLogowanie;
+
+        if(wyborLogowanie=='1') {
+            zarejestruj(uzytkownicy);
+            dopisanieDoPlikuUzytkownicy(uzytkownicy);
+        } else if(wyborLogowanie=='2') {
+            idZalogowanegoUzytkownika=logowanie(uzytkownicy);
+            if(idZalogowanegoUzytkownika!=0) {
+                wejdzDoMenuGlownego(adresaci, idZalogowanegoUzytkownika);
+            }
+        } else if(wyborLogowanie=='9') {
+            exit(0);
         }
     }
     return 0;
